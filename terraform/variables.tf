f38670c411b59785bd477ac50893d776ec8882fb
Fodor Dávid <david.fodor96@gmail.com>
2025-09-16 16:28:12 +0200

    add terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type    = string
+  default = "budget_planner-cluster"
+}
+
+variable "node_group_name" {
+  type    = string
+  default = "budget_planner-nodes"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
