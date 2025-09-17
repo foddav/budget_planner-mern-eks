3f1479a3c24b9118bd55e4271ea67f2c802cbaba
Fodor Dávid <david.fodor96@gmail.com>
2025-09-17 14:03:39 +0200

    fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,44 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = "guess_the_number-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = true
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = ">= 21.2.0"
 
   name    = var.cluster_name
   kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
@@ -37,9 +72,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes_host                   = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  kubernetes_token                  = data.aws_eks_cluster_auth.cluster.token
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
 }
 
 resource "helm_release" "mongodb" {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -16,7 +16,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0.2"
+      version = "~> 2.12"
     }
   }
 }
