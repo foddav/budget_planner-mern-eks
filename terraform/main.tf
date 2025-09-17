25d176fae7b913660d56b274253467289e906ca1
Fodor Dávid <david.fodor96@gmail.com>
2025-09-17 14:21:21 +0200

    fix main.tf names

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,11 +58,11 @@ resource "random_password" "mongodb_root_password" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
